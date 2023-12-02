4900414275ca73b007c53c038d1b5a813ad1bf3c
jasur <251tojiboyev@gmail.com>
2023-12-02 10:09:04 +0500

    uy_ishi2

---
+++ main.py
@@ -0,0 +1,9 @@
+massiv = [3, 7, 2, 5]
+total = 0
+index = 0
+for element in massiv:
+    total += massiv[index]
+    index += 1
+
+print("massivni ichidagi sonlar:", massiv)
+print("Umumiy yig'indisi:", total)
